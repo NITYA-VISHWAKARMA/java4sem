0ebd2fe8c5247223e45b428dcea190faf7d94291
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2026-04-27 15:16:16 +0530

    java program uploaded

---
+++ lec4.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+// public class lec4 {
+//     static int fibo(int n)
+//         if(n <=1) return n;
+//         return fibo(n-1) + fibo(n-2);
+//     }
+//       public static void main(String[] args) { 
+//         int n = 5;
+//         System.out.println(fibo(n)); 
+//       }  
+
+// public class recursionfunction {
+//     static int total = 0;
+//     static void subString(int[] arr,int i,int sum){
+//     if(i==arr.length){
+//         total+=sum;
+//         return;
+//     }
+//     subsetsum(arr, i+1, sum:sum+arr[i]);
+//     subsetsum(arr, i+1, sum);
+//     }
+
+// public static void main(String[] args) {
+//     int[] arr = {1,2,3};
+//     int i =0;
+//     int sum =0;
+//     subsetsum(arr,i,sum);
+//     System.out.println(total);
+// }
+// }
+
+public class recursionfunction {
+    static int subString(int[] arr,int i){
+        if(i==arr.length) return 0;
+        int x = arr[i]+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
