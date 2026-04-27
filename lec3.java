8e8696d3b45742175a5b20ea6dc6064627a8b9f7
NITYA-VISHWAKARMA <vishwakarmanitya559@gmail.com>
2026-04-27 15:15:57 +0530

    java program uploaded

---
+++ lec3.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+public class lec3{
+    public static void main(String[] args) {
+    //     String str = "abc";
+    //     for(int i=0;i<str.length();i++){
+    //         for(int j=i+1;j<str.length();j++){
+    //             System.out.println(str.substring(i,j));
+    //         }
+       
+    
+
+
+    // String str1 ="abc";
+    // int count =0;
+    // for(int i=0;i<str1.length();i++){
+    //         for(int j=i;j<str1.length();j++){
+    //            if(str1.charAt(i) == str1.charAt(j))
+    //            {
+    //             count++;
+    //            }
+    //         }
+    //     }
+    //     System.out.println(count);
+
+    //     String str1 = "a3bhj26fh";
+    //     char max = '0';
+    //     for(int i=0;i<str1.length();i++)
+    //     {
+    //         char ch = str1.charAt(i);
+    //         if(ch >='0'&& ch<='9' && ch > max)
+    //         {
+    //             max = ch;
+    //         }
+    //         System.out.println(max);
+    //     }
+
+
+    // String str3 = "Mango ";
+    // char rem = 'o';
+    // for (int i=0;i<str3.length();i++){
+    //     if(str3.charAt(i)==rem){
+    //     System.out.println(str3.replace('o', ' '));
+    //     }
+    // }
+
+    // String str4 = "abbbbccdeaa";
+    // String result = "";
+    //     result += str4.charAt(0);
+    //      for(int i=1;i<str4.length();i++){
+    //   if (str4.charAt(i) != str4.charAt(i-1)) {
+    //     result += str4.charAt(i);
+    //   }
+    //   System.out.println(result);
+    // }
+
+    Static void Printnumber(int n)
+    {
+        if(n>5)
+        {
+            return;
+        }
+        System.out.println(n);
+        Printnumber(n+1);
+    }
+    Public static void main(String[],args)
+    {
+        
+    }
+
+
+
+      }
+    }
+   
